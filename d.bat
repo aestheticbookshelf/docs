511ddcee75be1b04bdc71d58738eb63279eab3a8
aestheticbookshelf <aestheticbookshelf@gmail.com>
2020-02-18 07:18:40 +0100

    Docs structure

--- d.bat
+++ d.bat
@@ -1,7 +1,7 @@
 call taskkill /fi "WindowTitle eq http-server"
 
-call jsdoc -c jsdoc.config.json
+call jsdoc -c %1.jsdoc.config.json
 call ca
+cd docs\%1
 start http-server -p 3000
-cd ..
-
+cd ..\..\..
